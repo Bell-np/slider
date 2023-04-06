48b621b4149976b66513a64ac5cea37eb353000c
Eduard Mednikov <EduardMedh@yandex.ru>
2023-04-06 20:24:58 +0300

    Всё доделано(вроде без багов), остался рефакторинг

--- next.js
+++ next.js
@@ -30,12 +30,26 @@ const area = document.querySelector('#area');
 const time = document.querySelector('#time');
 const img = document.querySelector('#img-example');
 
+
 // Функция смены информации слайдера
-const setEntity = (index) => {
+const setEntity = (index, pastObject) => {
+    const past = document.querySelector(`#project${pastObject}`);
+    const project = document.querySelector(`#project${index}`);
+
+    const pastNextProject = document.querySelector(`#next-project-${pastObject}`);
+    const nextProject = document.querySelector(`#next-project-${index}`);
+
     city.innerHTML = entities[index].city;
     area.innerHTML = entities[index].area;
     time.innerHTML = entities[index].time;
     img.src = entities[index].img;
+
+    project.classList.add('active');
+    past.classList.remove('active');
+
+    nextProject.classList.add('next-project_active');
+    pastNextProject.classList.remove('next-project_active');
+
 }
 
 
@@ -49,11 +63,11 @@ let currentIndex = 0;
 leftArrow.addEventListener('click', () => {
     if (currentIndex <= 0) {
         currentIndex = 2;
-        setEntity(currentIndex);
+        setEntity(currentIndex, 0);
     }
     else {
         currentIndex -= 1;
-        setEntity(currentIndex);
+        setEntity(currentIndex, currentIndex + 1);
     }
 
 })
@@ -61,36 +75,45 @@ leftArrow.addEventListener('click', () => {
 rightArrow.addEventListener('click', () => {
     if (currentIndex < 2) {
         currentIndex += 1;
-        setEntity(currentIndex);
+        setEntity(currentIndex, currentIndex - 1);
     }
     else {
         currentIndex = 0;
-        setEntity(currentIndex);
+        setEntity(currentIndex, 2);
     }
 })
 
-
 // Кружочки между стрелками
-const nextProject1 = document.querySelector('#next-project-1').addEventListener('click', () => {
-    setEntity(0);
+const nextProject0 = document.querySelector('#next-project-0').addEventListener('click', () => {
+    setEntity(0, 1);
+    setEntity(0, 2);
+    currentIndex = 0;
 })
-const nextProject2 = document.querySelector('#next-project-2').addEventListener('click', () => {
-    setEntity(1);
+const nextProject1 = document.querySelector('#next-project-1').addEventListener('click', () => {
+    setEntity(1, 0);
+    setEntity(1, 2);
     currentIndex = 1;
 })
-const nextProject3 = document.querySelector('#next-project-3').addEventListener('click', () => {
-    setEntity(2);
+const nextProject2 = document.querySelector('#next-project-2').addEventListener('click', () => {
+    setEntity(2, 1);
+    setEntity(2, 0);
+    currentIndex = 2;
 })
 
 
 // Ссылки над слайдером
-const project1 = document.querySelector('#project1').addEventListener('click', () => {
-    setEntity(0);
+const project0 = document.querySelector('#project0').addEventListener('click', () => {
+    setEntity(0, 1);
+    setEntity(0, 2);
+    currentIndex = 0;
 })
-const project2 = document.querySelector('#project2').addEventListener('click', () => {
-    setEntity(1);
+const project1 = document.querySelector('#project1').addEventListener('click', () => {
+    setEntity(1, 0);
+    setEntity(1, 2);
     currentIndex = 1;
 })
-const project3 = document.querySelector('#project3').addEventListener('click', () => {
-    setEntity(2);
+const project2 = document.querySelector('#project2').addEventListener('click', () => {
+    setEntity(2, 1);
+    setEntity(2, 0);
+    currentIndex = 2;
 })
